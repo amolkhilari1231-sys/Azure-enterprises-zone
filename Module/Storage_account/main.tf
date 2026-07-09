34e3bee237c5d3cdf0311f70563aa166043f145d
Amol Khilari <amol.khilari1231@gmail.com>
2026-07-09 22:37:43 +0530

    added vm and nsg

--- Module/Storage_account/main.tf
+++ Module/Storage_account/main.tf
@@ -1,8 +1,9 @@
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = var.storageaccount
+  for_each = var.storageaccount
+
+  name                     = each.value.name
   resource_group_name      = each.value.resource_group_name
   location                 = each.value.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
 }
