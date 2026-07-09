cf121fcb5eb342d5c6de15055f9199cc7588fc0b
Amol Khilari <amol.khilari1231@gmail.com>
2026-07-10 00:01:07 +0530

    Added environment files

---
+++ Enviroment/dev/main.tf
@@ -0,0 +1,42 @@
+module "resource_group" {
+  source = "../../Module/resource_group"
+  rgs    = var.rgs
+
+}
+
+module "virtual_network" {
+  depends_on = [module.resource_group]
+  source     = "../../Module/virtual network"
+  vnet       = var.vnet
+}
+
+module "subnet" {
+  depends_on = [module.virtual_network]
+  source     = "../../Module/Azurerm_subnet"
+  subnet     = var.subnet
+}
+
+module "storage_account" {
+  depends_on     = [module.resource_group]
+  source         = "../../Module/Storage_account"
+  storageaccount = var.storageaccount
+}
+
+module "network_security_group" {
+  depends_on = [module.resource_group]
+  source     = "../../Module/Network_security_group"
+  nsg        = var.nsg
+}
+
+module "azurerm_nic" {
+  depends_on = [module.subnet]
+  source     = "../../Module/azurerm_nic"
+  nic        = var.nic
+  
+}
+
+module "virtual_machine" {
+  depends_on = [module.azurerm_nic]
+  source     = "../../Module/virtual_machine"
+  vms        = var.vms
+}
---
+++ Enviroment/dev/variable.tf
@@ -0,0 +1,78 @@
+variable "rgs" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+}
+variable "vnet" {
+  type = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    address_space       = list(string)
+  }))
+}
+
+variable "subnet" {
+  description = "Map of subnet definitions to create."
+  type = map(object({
+    name             = string
+    vnet_name        = string
+    rg_name          = string
+    address_prefixes = list(string)
+  }))
+}
+
+variable "storageaccount" {
+  description = "Map of storage account definitions to create."
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+  }))
+}
+
+variable "nsg" {
+  description = "Map of network security group definitions to create."
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+  }))
+}
+
+variable "nic" {
+  type = map(object({
+    nic_name                        = string
+    location                        = string
+    resource_group_name             = string
+    ip_config_name                  = string
+    subnet_id                       = string
+    private_ip_address_allocation   = string
+    private_ip_address              = string
+    public_ip_address_id            = string
+  }))
+}
+
+variable "vms" {
+  type = map(object({
+    vm_name                          = string
+    location                         = string
+    resource_group_name              = string
+    nic_key                          = string
+    vm_size                          = string
+    delete_os_disk_on_termination    = bool
+    delete_data_disks_on_termination = bool
+    publisher                        = string
+    offer                            = string
+    sku                              = string
+    version                          = string
+    os_disk_name                     = string
+    caching                          = string
+    create_option                    = string
+    managed_disk_type                = string
+    computer_name                    = string
+    admin_username                   = string
+    admin_password                   = string
+  }))
+}
